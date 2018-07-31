9499451b2b24dd5c34d26bed3c629a19f380c5dc
QuentinDll <quentin.delplace13@gmail.com>
2018-07-31 14:31:33 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Exercice 2</title>
+</head>
+<body>
+  <p>
+    <?php
+    $numb1 = 0;
+    $numb2 = 25;
+
+    while ($numb1 <= 20) {
+      echo $numb1 * $numb2 ." ";
+      echo $numb1 ++ ." ";
+    }
+    ?>
+  </p>
+</body>
+</html>
